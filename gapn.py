aa7c13699182ec9715598e7be340b2fde00a9227
arkanataya <78206974+arkanataya@users.noreply.github.com>
2023-02-18 09:40:19 +0700

    Update gapn.py

--- gapn.py
+++ gapn.py
@@ -4,3 +4,17 @@ command = sys.argv[1]
 package = sys.argv[2]
 if command == "install" :
   print("[Downloading]: Downloading Package")
+  url = 'http://i3.ytimg.com/vi/J---aiyznGQ/mqdefault.jpg'
+  r = requests.get(url)
+
+  with open('/Users/scott/Downloads/cat3.jpg', 'wb') as f:
+  f.write(r.content)
+
+  # Retrieve HTTP meta-data
+  print(r.status_code)
+  print(r.headers['content-type'])
+  print(r.encoding)
+  if r.status_code == 500 :
+    print("500: Server Error")
+  if r.status_code == 404
+    print("E: Package Now Found")
